dc6c14df2f16b197577d4b1a517ecc063667a500
pirolinthechihuahua <da25estrada@gmail.com>
2019-07-24 10:34:44 -0700

    dfghjkl

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,15 @@
 $( document ).ready(function() { //required for jQuery to work
 
 //all javascript and jquery code here
+// Get the modal
+var modal = document.getElementById('id01');
 
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
 
 
 
